6a4eb75ab2771a6b4878026ed22ccf035d484bd9
oliveiraneto <90579809+oliveiraneto@users.noreply.github.com>
2023-09-17 02:22:05 -0300

    Update circuito_de_led.ino

--- circuito_de_led.ino
+++ circuito_de_led.ino
@@ -1,5 +1,4 @@
-// C++ code
-//
+//Aluno: Sebastião Oliveira Silva Neto - 2011478
 int counter;
 
 void setup()
@@ -10,11 +9,11 @@ void setup()
 void loop()
 {
   digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000); // Wait for 1000 millisecond(s)
+  delay(1000); 
   digitalWrite(LED_BUILTIN, LOW);
-  delay(1000); // Wait for 1000 millisecond(s)
+  delay(1000); /
   digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000); // Wait for 1000 millisecond(s)
+  delay(1000); 
   digitalWrite(LED_BUILTIN, LOW);
   for (counter = 0; counter < 10; ++counter) {
     digitalWrite(LED_BUILTIN, HIGH);
